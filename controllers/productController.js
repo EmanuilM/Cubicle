726907938b54f4c316c7963989e08deaa18f39e0
ExGiX <emanuilmladenov7@gmail.com>
2021-04-12 13:53:51 +0300

    save cubes to db

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,6 +2,9 @@ const {Router} = require('express');
 const cubeMaker = require('../models/cube');
 const uniqueId = require('uniqid');
 const router = Router();
+const fs = require('fs');
+const db = require('../config/db.json');
+const path = require('path');
 
 //Setup create page view
 
@@ -13,10 +16,15 @@ router.get('/create' , (req,res) => {
 
 router.post('/create' , (req,res) => { 
     const cube = new cubeMaker(Object.assign(req.body , {id : uniqueId()}));
-    res.send(cube);
+    console.log(cube);
+    db.push(cube);
+    fs.writeFile(path.join(__dirname , '../config/db.json') , JSON.stringify(db) , (err) => { 
+        if(err) { 
+           return console.log(err);
+        }
+    })
+    res.status(201).redirect('/');
 });
-
-
 
 module.exports = router;
 
