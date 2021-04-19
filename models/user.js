b7c3408ba98d6cd60d1519b6a77c5cad95a334db
ExGiX <emanuilmladenov7@gmail.com>
2021-04-19 19:06:34 +0300

    update user schema

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     username: { 
         required:true,
         type:String,
