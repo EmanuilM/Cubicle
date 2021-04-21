b3e7518a0d5128920bb5b24934ca470ccf0e4c6a
ExGiX <emanuilmladenov7@gmail.com>
2021-04-21 19:34:39 +0300

    fix

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,7 +5,6 @@ const userModel = require('../models/user');
 const productService = require('../services/productService');
 const isGuest = require('../middlewares/guest');
 const isAuth = require('../middlewares/isAuth');
-const test = require('../middlewares/test');
 
 
 router.get('/create', isAuth, (req, res) => {
@@ -30,16 +29,16 @@ router.get('/details/:id', async (req, res) => {
     try {
         const cube = await productService.getAccessories(req.params.id);
         const creatorId = cube.creator;
-            const isAuthor = creatorId == req.user._id
-            res.render('updatedDetailsPage', { cube , isAuthor});
-        
+        const isAuthor = creatorId == req.user._id
+        res.render('updatedDetailsPage', { cube, isAuthor });
+
     } catch {
         res.redirect('/auth/login');
     }
 
 });
 
-router.get('/edit/:id', isAuth, test , async (req, res) => {
+router.get('/edit/:id', isAuth, async (req, res) => {
     const cube = await cubeModel.findOne({ _id: req.params.id });
 
     res.render('editCubePage', { cube });
